52236c13b4d64f9a59a84f425685c1bfae87778e
Zachary Atha <zacharyatha@gmail.com>
2022-06-02 20:10:02 -0400

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,74 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  console.log('EXPECTED: ', JSON.stringify(obj));
+  var result = '';
+
+  //BASE CASES
+  if (typeof(obj) === 'number') {
+    result += obj;
+  }
+
+  if (typeof(obj) === 'boolean') {
+    result += obj;
+  }
+
+  if (typeof(obj) === 'string') {
+    result += '"' + obj + '"';
+  }
+
+  if (typeof(obj) === 'undefined') {
+    return undefined;
+  }
+
+  if (typeof(obj) === 'function') {
+    return undefined;
+  }
+  if (obj === null) {
+    result += 'null';
+    return result;
+  }
+
+  if (Array.isArray(obj)) {
+    //    [1, 'two', null]
+    result += '[';
+    for (var i = 0; i < obj.length; i++) {
+      if (obj[i] === undefined) {
+        return undefined;
+      }
+      if (i === obj.length - 1) {
+        result += stringifyJSON(obj[i]);
+      } else {
+        result += stringifyJSON(obj[i]);
+        result += ',';
+      }
+    }
+    result += ']';
+    return result;
+  }
+
+  if (typeof(obj) === 'object') {
+    if (Object.keys(obj).length === 0) {
+      result += '{}';
+      return result;
+    }
+    result += '{';
+
+    for (var key in obj) {
+      var value = obj[key];
+      if (stringifyJSON(value) === undefined) {
+        continue;
+      }
+      result += '"' + key + '"';
+      result += ':';
+      result += stringifyJSON(value);
+      result += ',';
+    }
+    if (result.length > 1) {
+      result = result.slice(0, -1);
+    }
+    result += '}';
+  }
+
+  return result;
 };
